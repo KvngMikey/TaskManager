c4be8824ab8d582e1b049d6f184eaf4a38ebc33b
Michael Ariwaodo <contact.mikeariwaodo@gmail.com>
2024-11-08 00:36:54 +0100

    feat: filters

--- src/app/@components/task-list/task-list.component.ts
+++ src/app/@components/task-list/task-list.component.ts
@@ -10,21 +10,31 @@ import { TaskFormComponent } from '../task-form/task-form.component';
 import { TaskService } from '../../@core/services/task.service';
 import { Subscription } from 'rxjs';
 import { Task } from '../../@core/interfaces';
+import { MatMenuModule } from '@angular/material/menu';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule, TaskFormComponent],
+  imports: [CommonModule, TaskFormComponent, MatMenuModule, FormsModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class TaskListComponent implements OnInit, OnDestroy {
   tasks: Task[] = [];
+  selectedStatus: string = '';
+  selectedPriority: string = '';
+  filteredTasks: Task[] = [];
+  statusOptions: string[] = ['Pending', 'In Progress', 'Completed'];
+  priorityOptions: string[] = ['Low', 'Medium', 'High'];
 
   private subscription: Subscription = new Subscription();
 
-  constructor(private taskService: TaskService, private cdr: ChangeDetectorRef) {}
+  constructor(
+    private taskService: TaskService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.loadTasks();
@@ -35,8 +45,9 @@ export class TaskListComponent implements OnInit, OnDestroy {
       this.subscription.add(
         this.taskService.tasks$.subscribe((tasks) => {
           this.tasks = tasks;
+          this.filterTasks();
         })
-      ) 
+      );
       this.cdr.detectChanges();
     }
   }
@@ -49,6 +60,18 @@ export class TaskListComponent implements OnInit, OnDestroy {
   deleteTask(task: Task): void {
     this.taskService.deleteTask(task.id);
     this.loadTasks();
+  }
+
+  filterTasks(): void {
+    this.filteredTasks = this.tasks.filter((task) => {
+      const statusMatch = this.selectedStatus
+        ? task.status === this.selectedStatus
+        : true;
+      const priorityMatch = this.selectedPriority
+        ? task.priority === this.selectedPriority
+        : true;
+      return statusMatch && priorityMatch;
+    });
   }
 
   ngOnDestroy(): void {
